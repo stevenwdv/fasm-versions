d6e1f9f16f2bb677ff9211db1018be47a408c3c6
StevenWdV <stevenwdv@gmail.com>
2022-06-06 15:13:09 +0200

    Add code

---
+++ README.md
@@ -0,0 +1,5 @@
+# flatassembler version list
+
+fasm version list used in [stevenwdv/setup-fasm](https://github.com/stevenwdv/setup-fasm), with workflow checking for new versions. [`fasm_versions.json`](https://github.com/stevenwdv/fasm-versions/blob/main/fasm_versions.json) contains the list of versions.
+
+This contains unpacked TypeScript, which is kind of ugly, but I don't want to commit JS if I don't have to and including this with ts-node works.
---
+++ src/check-new-versions.ts
@@ -0,0 +1,94 @@
+#!/usr/bin/env ts-node
+
+import crypto from 'node:crypto';
+import fs from 'node:fs';
+import {IncomingMessage} from 'node:http';
+import https from 'node:https';
+import path from 'node:path';
+import process from 'node:process';
+import consumers from 'node:stream/consumers';
+import {pipeline} from 'node:stream/promises';
+
+import {FasmData, FasmEditionStr, getUrls, PlatformStr} from './version-data';
+import dataRaw from '../fasm_versions.json';
+
+const data = dataRaw as FasmData;
+
+async function main() {
+	console.log('downloading fasm page');
+	const page = await consumers.text(await httpsGet(pageUrl));
+
+	function getVersions(pattern: RegExp): Set<string> {
+		return new Set([...page.matchAll(pattern)].map(m => m.slice(1).filter(g => g)[0]!));
+	}
+
+	const newVersionsTotal = (await Promise.all(
+		  [...versionPatterns.entries()].map(async ([edition, versionPattern]) => {
+			  const foundVersions = getVersions(versionPattern);
+			  if (!foundVersions.size) throw new Error(`Found 0 versions for ${edition}`);
+			  const newVersions = [...foundVersions].filter(foundVersion =>
+					!data.editions[edition].versions.find(v => v.name === foundVersion));
+			  if (newVersions.length)
+				  console.info(`found new version(s) for ${edition}: ${newVersions.join(', ')}`);
+
+			  const versions = (await Promise.all(newVersions.map(async version => ({
+				  name: version,
+				  hashes: Object.fromEntries(await Promise.all(platforms.map(async platform =>
+						[platform, await getHash(new URL(getUrls[edition]({name: version}, platform)[0]!))] as const))),
+			  })))).sort();
+			  for (const version of versions)
+				  data.editions[edition].versions.unshift(version);
+			  return newVersions.map(version => ({edition, version}));
+		  }))).flat();
+
+	if (newVersionsTotal.length) {
+		fs.writeFileSync(path.resolve(__dirname, '../fasm_versions.json'), JSON.stringify(data, undefined, '\t') + '\n');
+		console.info(`added ${newVersionsTotal.length} new version(s) to fasm_versions.json`);
+		console.log(`added [[${newVersionsTotal.map(({edition, version}) => `${edition} ${version}`).join(', ')}]]`);
+	} else console.info('no new versions found');
+}
+
+const pageUrl = new URL('https://flatassembler.net/download.php');
+
+const versionPatterns = new Map<FasmEditionStr, RegExp>([
+	['fasm1', /\bfasm-(\S+)\.(?:tgz|tar\.gz)\b/g],
+	['fasmg', /\bfasmg\.(\S+)\.zip\b/g],
+]);
+
+const platforms: PlatformStr[] = ['windows', 'linux', 'unix'];
+
+const hashes = new Map<string, Promise<string>>();
+
+async function getHash(url: URL): Promise<string> {
+	if (!hashes.has(url.href)) hashes.set(url.href, (async () => {
+		const res    = await httpsGet(url);
+		const hasher = crypto.createHash('BLAKE2b512').setEncoding('hex');
+		await pipeline(res, hasher);
+		console.log(`downloaded ${url.href}`);
+		return hasher.read() as string;
+	})());
+	return await hashes.get(url.href)!;
+}
+
+function httpsGet(url: URL): Promise<IncomingMessage> {
+	return new Promise((resolve, reject) =>
+		  // eslint-disable-next-line no-promise-executor-return
+		  void https.get(url, url.href !== pageUrl.href ? {
+			  // Prevent 'unsafe legacy renegotiation disabled' error because of unpatched flatassembler.net server
+			  secureOptions: 0x40000000, /*SSL_OP_NO_RENEGOTIATION*/
+		  } : {}, res => {
+			  if (res.statusCode !== 200)
+				  reject(new Error(`failed to download ${url.href}: HTTP ${res.statusCode!} ${res.statusMessage!}`));
+			  else resolve(res);
+		  }).on('error', err => reject(new Error(`failed to download ${url.href}`, {cause: err}))),
+	);
+}
+
+void (async () => {
+	try {
+		await main();
+	} catch (error) {
+		console.error(error);
+		process.exit(1);
+	}
+})();
